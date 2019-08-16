88f2cb085e60383cd67003fca80092bcd11b3edf
Marjan Jovanovic <makij7@yahoo.com>
2019-08-16 23:34:36 +0200

    V2

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navigation',
@@ -7,15 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  activetab = 'home';
+  // activetab = 'home';
 
   constructor() { }
 
   ngOnInit() {
-  }
 
-  getActiveTab(tabname: string){
-    this.activetab = tabname;
+    (<any>$)(document).ready(function () {
+
+      /*Responsive Navigation*/
+      (<any>$)('#nav-mobile').html((<any>$)('#nav-main').html());
+      (<any>$)('#nav-trigger span').on('click', function () {
+        if ((<any>$)('nav#nav-mobile ul').hasClass('expanded')) {
+          (<any>$)('nav#nav-mobile ul.expanded').removeClass('expanded').slideUp(250);
+          (<any>$)(this).removeClass('open');
+        } else {
+          (<any>$)('nav#nav-mobile ul').addClass('expanded').slideDown(250);
+          (<any>$)(this).addClass('open');
+        }
+      });
+
+      (<any>$)('#nav-mobile').html((<any>$)('#nav-main').html());
+      (<any>$)('#nav-mobile ul a').on('click', function () {
+        if ((<any>$)('nav#nav-mobile ul').hasClass('expanded')) {
+          (<any>$)('nav#nav - mobile ul.expanded').removeClass('expanded').slideUp(250);
+          (<any>$)('#nav - trigger span').removeClass('open');
+        }
+      });
+
+      // /* Sticky Navigation */
+      // if (!!(<any>$).prototype.stickyNavbar) {
+      //   (<any>$)('#header').stickyNavbar();
+      // }
+
+      // (<any>$)('#content').waypoint(function (direction) {
+      //   if (direction === 'down') {
+      //     (<any>$)('#header').addClass('nav-solid fadeInDown');
+      //   }
+      //   else {
+      //     (<any>$)('#header').removeClass('nav-solid fadeInDown');
+      //   }
+      // });
+    });
   }
+  // getActiveTab(tabname: string){
+  //   this.activetab = tabname;
+  // }
 
 }
